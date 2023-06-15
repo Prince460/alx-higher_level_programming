f368329512a683c93c4828f4e5cdb0fa13574023
Princewill Chimdi <princeblogging@gmail.com>
2023-06-14 18:23:17 -0700

    Write a function that replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+
+    """Set to Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+
+    return (a_dictionary)
